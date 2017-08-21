e14876e18876376e14bc7aafb6467b600b699390
Tirthpancholi <30971854+Tirthpancholi@users.noreply.github.com>
2017-08-21 23:22:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
 console.log('Loaded!');
 
-//chnage the text to the main-text div
+//Change the text to the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = "Hello fellas!!!";
 
+// Move the image to right
