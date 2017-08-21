d85aef499b875de0678094f1877320e3c0eac17d
Tirthpancholi <30971854+Tirthpancholi@users.noreply.github.com>
2017-08-21 23:36:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,5 +7,5 @@ element.innerHTML = "Hello fellas!!!";
 // Move the image to right
 var img = document.getElementById('madi');
 img.onclick = function () {
-    img.style.marginleft = '80px';
+    img.style.marginLeft = '80px';
 };
