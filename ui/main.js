aa1475ae458606d7e52ceaa235c55bc998a8ae36
Tirthpancholi <30971854+Tirthpancholi@users.noreply.github.com>
2017-08-25 18:28:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -50,6 +50,8 @@ submit.onclick = function () {
     // Not done yet
     };
     // Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://tirthinbusn.imad.hasura-app.io/submit-name?name='+ name, true);
     request.send(null); 
 };
