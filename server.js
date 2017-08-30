08100bb1b320e861661f6543138f3d8d9effa07e
Tirthpancholi <30971854+Tirthpancholi@users.noreply.github.com>
2017-08-30 22:11:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+
 var counter = 0;
 app.get('/counter', function(req, res)
 { counter = counter + 1;
