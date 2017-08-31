1930402b6d92d57f95d3ad7b42b925c64189b8c8
Tirthpancholi <30971854+Tirthpancholi@users.noreply.github.com>
2017-08-31 23:24:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
+var config = {
+    user: 'tirthinbusn',
+    database: 'tirthinbusn',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -79,8 +87,19 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-
+var pool = new pool(config);
+app.get('/test-db', function (req, res){
+    //make a select request
+    //return a response to the results
+    pool.query('SELECT * FROM test', function (err, result) {
+        if (err) {
+            res.ststus(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+        
+    });
+});
 
 var counter = 0;
 app.get('/counter', function(req, res)
