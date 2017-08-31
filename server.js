a176f4a902040e3398b352c7a2f9e225aafc1507
Tirthpancholi <30971854+Tirthpancholi@users.noreply.github.com>
2017-08-31 23:39:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var config = {
     user: 'tirthinbusn',
     database: 'tirthinbusn',
     host: 'db.imad.hasura-app.io',
-    port: '8080',
+    port: '5432',
     password: process.env.DB_PASSWORD
 };
 
