503fcc80fa85c6ef9afe9b9bd3874dad03ca2760
Tirthpancholi <30971854+Tirthpancholi@users.noreply.github.com>
2017-09-03 14:48:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.get('/articles/:articleName', function(req, res){
     // articleName == article-one
     //articlename == {} content object for article one
     
-    Pool.query("Select * from WHERE title = " + req.params.articleName, function (err, result){
+    pool.query("Select * from WHERE title = " + req.params.articleName, function (err, result){
        if(err){
            res.status(500).send(err.toString());
        }else {
